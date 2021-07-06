5c6f356c34676975bb3f6f5d4da9fdc65bd28883
carlos1205 <carloslima68.cs@gmail.com>
2021-07-06 16:10:48 -0300

    create accordion from eixos

---
+++ src/sections/Eixos/Accordion/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div :id="`accordion-${title}`" class="accordion" role="tablist">
+        <b-card 
+            class="mb-1"
+            no-body
+            v-for="(item, index) in itens"
+            :key="index"
+        >
+            <b-card-header header-tag="header" class="p-0" role="tab">
+                <b-button 
+                    v-b-toggle="`accordion-${title}-${index}`"
+                    block
+                >
+                    {{item.name}}
+                </b-button>
+            </b-card-header>
+            <b-collapse
+                :id="`accordion-${title}-${index}`"
+                accordion="`accordion-${title}-${item.name}`"
+                role="tabpanel"
+            >
+                <b-card-body>
+                    <b-card-text
+                        v-for="(paragraph, number) in item.description"
+                        :key="`${title}-${index}-P${number}`"
+                    >
+                        {{paragraph}}
+                    </b-card-text>
+                </b-card-body>
+            </b-collapse>
+        </b-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            initList: Array
+        },
+        data: function() {
+            return ({
+                itens: this.initList
+            });
+        }
+    }
+</script>
